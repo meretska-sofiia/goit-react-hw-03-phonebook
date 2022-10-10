996bbc06425c2967c8ce92082a53073d6118c33f
meretska-sofiia <meretska.sofiia@gmail.com>
2022-10-10 22:04:05 +0300

    homework #3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,6 +11,20 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleAddContactClick = ({ name, number }) => {
     const nameArray = this.state.contacts.map(({ name }) => name);
 
